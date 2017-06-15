a13fb87b7f0952cb99bd8bd9fd829fdc2c17aeaa
tsaunders <tsaundersjr@gmail.com>
2017-06-15 11:51:46 -0400

    Added Date Time UTC Display

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -11,6 +11,13 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World Dev Branch Test");
+            Console.WriteLine(DisplayDateTime());
+            Console.ReadLine();
+        }
+
+        static string DisplayDateTime()
+        {
+            return DateTime.UtcNow.ToString();
         }
     }
 }
